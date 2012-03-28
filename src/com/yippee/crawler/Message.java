40e5aae9a0a9c8f4fb8fd8ba24e82e3311e33daf
nvasilakis <nikos.ailo@gmail.com>
2012-03-27 21:29:36 -0400

    Update Message class and tests

--- src/com/yippee/crawler/Message.java
+++ src/com/yippee/crawler/Message.java
@@ -2,11 +2,16 @@ package com.yippee.crawler;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 /**
  * Different components of the crawler pass Message objects around. Messages
  * always needs a (predefined) type, a URL, a timestamp and a number of
- * directives.
+ * directives. The url is broken into a number of pieces for easy retrieval.
+ *
+ * From the nature of the crawler a message is only created to encapsulate
+ * information for a specific url and pass them around.
  */
 public class Message {
     /**
@@ -14,37 +19,332 @@ public class Message {
      */
     static Logger logger = Logger.getLogger(Message.class);
     /**
+     * We handle solely http; we return Type.NOX o/w
+     */
+    private String protocol;
+    /**
+     * The port, usually 8080, IF IT IS ANOTHER PORT WE (should) IGNORE THE URL,
+     * returning Type.NOX
+     *
+     * Note, port = 0 means the user did not define port, assuming 80
+     */
+    private int port;
+    /**
+     * The host is of a the domain name (containing all levels) for instance:
+     * www.domain.com, gr.ubuntu.org, sub.88.de
+     */
+    private String host;
+    /**
+     * Last file .html, html or other extensions
+     */
+    private String file;
+    /**
+     * The /actual/path/to/the/resource/ just before the last file or action
+     * and after
+     */
+    private String path;
+    /**
+     * The query parameters are pretty useless at that point
+     */
+    private String params;
+    /**
      * It encapsulates status information or progress condition. This can be
      * either:
-     *  *   "NEW" for new messages,
-     *  *   "UPD" for urls with updated  info (like robots.txt data),
-     *  *   "FIN" for messages (once used, discard) and
-     *  *   "NOX" for invalid messages
+     *  *   "NEW" for new urls,
+     *  *   "UPD" for urls with updated  info (like robots.txt),
+     *  *   "FIN" for the last message and
+     *  *   "NOX" for invalid urls
      */
     public static enum Type {NEW, UPD, FIN, NOX}
     /**
-     * One of the types defined above
+     * One of the types defines above
      */
     private Type type;
+    /**
+     * The crawl delay is fed from robots.txt, if such a resource exists; if
+     * not, the crawlDelay is 0.
+     */
+    private int crawlDelay;
+    /**
+     * The list of disallow directives -- afterwards, the only only thing to do
+     * for str : disallow {if request.contains(str) abort}
+     */
+    private ArrayList<String> disallow;
+    /**
+     * The timestamp in seconds the latest hit to the same server was done
+     */
+    private int timestamp;
+    /**
+     * The URL is not in the robots.txt disallow directives.
+     */
+    private boolean isAllowed = true;
+
 
 
     /**
-     * Default constructor
+     * The default constructor. The url is expected to be of the form:
+     * http://domain[/path/to][/action[.extension][?parameters]]
+     * sections in [] are not required
+     *
+     * @param spec the given url as a String
+     * @throws MalformedURLException
      */
-    public Message(){
-        // Use dynamic level, push this to the outermost class
+    public Message(String spec) throws MalformedURLException{
         PropertyConfigurator.configure("log/log4j.properties");
-        // Create a new message and log it
-        logger.info("Creating new message");
+        
+        //1. protocol
+        //2. type
+        if (spec.startsWith("http://")) {
+            protocol = "http";
+            spec = spec.substring(7);
+            type = Type.NEW;
+        } else {
+            type = Type.NOX;
+            return;
+        }
+        String tempPath = "";
+        //3. query
+        params = "";
+        if (spec.contains("?")) {
+            params = spec.split("\\?")[1];
+            spec = spec.split("\\?")[0];
+        }
+        //4. host + path
+        host = "";
+        path = "";
+        file = "";
+        if (spec.contains("/")){
+            host = spec.split("/")[0];
+            path = "/"+spec.split("/",2)[1];
+            //5. file
+            int  slash  = path.lastIndexOf("/");
+            int  dot    = path.lastIndexOf(".");
+            // if it is not a file ok -- maybe it is a service
+            if (slash > dot) {
+                if (slash+1>=path.length()) { //this/is/a/dir/
+                    //do nothing, we are initialized correctly
+                } else { //this/is/a/service
+                    file = path.substring(slash+1,path.length());
+                    path = path.substring(0,slash+1);
+                }
+                // it is a file -- MAYBE keep this info to
+                // check .html .html .xhtml .xml .rss .atom
+            } else {
+                if (slash>0) {
+                    file = path.substring(slash+1,path.length());
+                    path = path.substring(0,slash+1);
+                } else {
+                    file = path.substring(slash+1,path.length());
+                    path = path.substring(0,slash+1);
+                }
+            }
+        } else {
+            host = spec;
+            path = "/";
+        }
+        port = 0; //6. domain + port
+        if (host.contains(":")){
+            try {
+                port = Integer.parseInt(host.split(":")[1]);
+                host = host.split(":")[0];
+            } catch (NumberFormatException e) {
+                type = Type.NOX;
+            }
+        }
+        if (type == Type.NOX) {
+            throw new MalformedURLException();
+        } else {
+            log();
+        }
     }
 
     /**
-     * This is just to test the logger -- it works!
-     *
-     * @param args
+     * Log the message
      */
-    public static void main(String[] args){
-        Message ms = new Message();
+    public void log() {
+        logger.info("Message url: " + getURL());
+    }
+
+
+    /**
+     * Get the protocol -- it is only http at that point
+     *
+     * @return "http"
+     */
+    public String getProtocol() {
+        return protocol;
+    }
+
+    /**
+     * Get the port, usually 80.
+     * NOTE, IF PORT returns 0, then the requester SHOULD ASSUME PORT 80.
+     * this is only for testing purposes, where we need to be able to make
+     * a distinction between given AURLs with port and without port
+     * (for assert equals)
+     *
+     * @return the port integer
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * Get the domain host
+     *
+     * @return the host string
+     */
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * Get the final part of the path, that is the file or service to be
+     * invoked, if such a file or service exists, "" o/w
+     *
+     * @return the file/service string
+     */
+    public String getFile() {
+        return file;
+    }
+
+    /**
+     * Get the full path, except filename/service
+     *
+     * @return the path string
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Get the request query parameters as a whole string. Due to space
+     * considerations, these are not broken into <key,value> pairs.
+     *
+     * @return the query parameters string
+     */
+    public String getParams() {
+        return params;
+    }
+
+    /**
+     * Get the augmented type.
+     *
+     * @return augmented type, in the form of a constant
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Set the augmented type.
+     *
+     * @param type augmented type, in the form of a constant
+     */
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * Returns getPath() in the style of the URL class, which is AURL-equivalent
+     * to getPath() + getFile()
+     *
+     * @return the full path containing /path/to and /file.xtnsion
+     */
+    public String getOldPath(){
+        //System.out.println("\t"+getPath()+"|"+getFile());
+        String s = getPath()+getFile();
+        return s;
+    }
+
+    /**
+     * Get the full URL, containing all information as a concatenated string
+     *
+     * @return the full url string
+     */
+    public String getURL(){
+        String url = "" + protocol + "://" + host;
+        url += (port==0)? "" : ":"+port;
+        url += path+file;
+        url += (params.equals(""))? "" : "?"+params;
+        return url;
+    }
+
+    /**
+     * Gets the disallow directives for this url -- if directives are null it
+     * means the domain has no such directives
+     *
+     * @return the disallow directives array list
+     */
+    public ArrayList<String> getDisallow() {
+        return disallow;
+    }
+
+    /**
+     * Sets the disallow directives for this url -- if directives are null it
+     * means the domain has no such directives
+     *
+     * @param disallow the disallow directives array list to be inserted
+     */
+    public void setDisallow(ArrayList<String> disallow) {
+        this.disallow = disallow;
+    }
+
+    /**
+     * Gets the crawl delay for this url -- if such a directive does not exist
+     * then the delay is by default 0 (the upper classes take care of how to
+     * interpret this value, as usually 0 means crawl at a minimum period of 10
+     * seconds).
+     *
+     * @return the crawl delay
+     */
+    public int getCrawlDelay() {
+        return crawlDelay;
+    }
+
+    /**
+     * Sets the crawl delay for this url -- if such a directive does not exist
+     * then the delay is 0
+     *
+     * @param crawlDelay the crawl delay in seconds
+     */
+    public void setCrawlDelay(int crawlDelay) {
+        this.crawlDelay = crawlDelay;
+    }
+
+    /**
+     * Get the timestamp in seconds
+     *
+     * @return the timestamp
+     */
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Set the timestamp in seconds
+     *
+     * @param timestamp the timestamp in seconds
+     */
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * Check if the URL is allowed to be crawled or not
+     *
+     * @return true if allowed, false o/w
+     */
+    public boolean isAllowed() {
+        return isAllowed;
+    }
+
+    /**
+     * Set whether the URL is allowed to be crawled or not
+     *
+     * @param allowed true if allowed, false o/w
+     */
+    public void setAllowed(boolean allowed) {
+        isAllowed = allowed;
     }
 
 }
--- test/com/yippee/crawler/MessageTest.java
+++ test/com/yippee/crawler/MessageTest.java
@@ -5,17 +5,195 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MessageTest {
-	Message m;
+    private String[] urls = {
+            "http://n.v.a.s.i.ak.is/~nvas/something/Africa.html",
+            "http://domain:8080/./Americas.html",
+            "http://www.seas.upenn/../AsiaPacific.html",
+            "http://www.cis.upenn.edu/~nvas/crawltest/index/./ea.html",
+            "http://www.cis.upenn.edu/~nvas/crawltest/index/",
+            "http://www.cis.upenn.edu/~nvas/crawltest/index", //without trailing / !!
+            "http://www.cis.upenn.edu/~nvas/crawltest/index/whatevah/../ea.html",
+            "http://www.cis.upenn.edu/~nvas/crawltest/index/ea.html",
+//            "http://we.com",// we don't care if it's equal to "http://we.com/",
+            "http://we.com/index.php",
+            "http://we.com/index",
+            "http://we.com/index/",
+//            "http://crawltest.cis.upenn.edu/"
+    };
 
-	@Before
-	public void setUp(){
-		m = new Message();
-	}
+    private String[] malformed = {
+            "https://we.com/index",
+            //"http://we.com///index",  // this is not malformed
+            "http://we.com:what/index"
+    };
 
-	@Test
-	public void testMessageConstructor(){
-		assertTrue(m instanceof Message);
-	}
+    /**
+     * Test well-formed urls; these should all create messages with no problem
+     */
+    @Test
+    public void testURLs() {
+        boolean test = true;
+        for (String url : urls) {
+            try {
+                if ((new Message(url).getType() == Message.Type.NEW)) {
+                    if (!url.equals(new Message(url).getURL())) {
+                        System.out.println("["+new Message(url).getURL()+"]");
+                        test = false;
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                test = false;
+            }
+        }
+        assertTrue(test);
+    }
+
+    /**
+     * Test malformed urls; these should throw exception
+     */
+    @Test
+    public void testMalformed() {
+        boolean test = true;
+        for (String url : malformed) {
+            try {
+                if ((new Message(url).getType() != Message.Type.NOX)) {
+                    System.out.println(new Message(url).getURL());
+                    test = false;
+                }
+            } catch (MalformedURLException e) {
+                //e.printStackTrace(); // we know that it is malformed!
+                test = true;
+            }
+        }
+        assertTrue(test);
+    }
+
+    /**
+     * Test that the host is equivalent to the getHost() method of the url class
+     */
+    @Test
+    public void testHostEq() {
+        boolean test = true;
+        for (String url : urls) {
+            try{
+                if (!(new URL(url)).getHost().equals((new Message(url).getHost()))){
+                    test = false;
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                test = false;
+            }
+        }
+        assertTrue(test);
+    }
+
+    /**
+     * Test that the oldPath method can provide the getPath() equivalent of the url class
+     */
+    @Test
+    public void testPathEq() {
+        boolean test = true;
+        for (String url : urls) {
+            try{
+                if (!(new URL(url)).getPath().equals((new Message(url).getOldPath()))){
+                    System.out.println((new URL(url)).getPath() +"|"+(new Message(url)).getOldPath());
+                    test = false;
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                test = false;
+            }
+        }
+        assertTrue(test);
+    }
+
+    /**
+     * Make sure that the message type works
+     */
+    @Test
+    public void testType(){
+        Message message = null;
+        try {
+            message = new Message("http://www.example.com");
+            message.setType(Message.Type.UPD);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        assertEquals(Message.Type.UPD, message.getType());
+    }
+
+    /**
+     * Test a number of variations for short paths, these should work ok
+     */
+    @Test
+    public void testGetPath() {
+        boolean result = true;
+        String s1 = "http://we.com/index";
+        String s2 = "http://we.com/index/";
+        String s3 = "http://we.com/";
+        String s4 = "http://we.com";
+        Message message1 = null; Message message2 = null;
+        Message message3 = null; Message message4 = null;
+        try {
+            message1 = new Message(s1);
+            message2 = new Message(s2);
+            message3 = new Message(s3);
+            message4 = new Message(s4);
+            String path1 = message1.getPath();
+            String path2 = message2.getPath();
+            String path3 = message3.getPath();
+            String path4 = message4.getPath();
+            if (!path1.equals("/") || !path2.equals("/index/") || !path3.equals("/") || !path4.equals("/")) {
+                System.out.println(path1 +"|"+path2 + "|" +path3 +"|"+path4);
+                result = false;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            result = false;
+        }
+        assertTrue(result);
+
+    }
+
+    /**
+     * Test a number of variations for short paths, these should work ok
+     */
+    @Test
+    public void testFile() {
+        boolean result = true;
+        String s1 = "http://we.com/index";
+        String s2 = "http://we.com/index/";
+        String s3 = "http://we.com/";
+        String s4 = "http://we.com";
+        Message message1 = null; Message message2 = null;
+        Message message3 = null; Message message4 = null;
+        try {
+            message1 = new Message(s1);
+            message2 = new Message(s2);
+            message3 = new Message(s3);
+            message4 = new Message(s4);
+            String path1 = message1.getFile();
+            String path2 = message2.getFile();
+            String path3 = message3.getFile();
+            String path4 = message4.getFile();
+            if (!path1.equals("index") || !path2.equals("") || !path3.equals("") || !path4.equals("")) {
+                System.out.println(path1 +"|"+path2);
+                result = false;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            result = false;
+        }
+        assertTrue(result);
+
+    }
 
 }
