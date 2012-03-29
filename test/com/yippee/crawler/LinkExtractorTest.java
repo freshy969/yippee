e632110bd187215e12ae9a0c804e19243f72cda0
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-03-28 22:41:45 -0400

    added notes

--- test/com/yippee/crawler/LinkExtractorTest.java
+++ test/com/yippee/crawler/LinkExtractorTest.java
@@ -2,13 +2,20 @@ package com.yippee.crawler;
 
 import static org.junit.Assert.*;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class LinkExtractorTest {
 
+	URLFilter filter;
+	
 	@Before
 	public void setUp() throws Exception {
+		filter = new URLFilter();
 	}
 
 	@Test
@@ -16,4 +23,18 @@ public class LinkExtractorTest {
 		fail("Not yet implemented");
 	}
 
+	
+	public void testRelativeUrlsWithSlash(){
+		List<String> extractedURLs = new ArrayList<String>();
+		extractedURLs.add("/index.html");
+		extractedURLs.add("index.html");
+		
+		//List<URL> absolutes = filter.makeAbsolute("http","crawltest.cis.upenn.edu" , extractedURLs);
+		//assertEquals(2, absolutes.size());
+		
+		
+//		for(URL url : absolutes){
+//			assertEquals("http://crawltest.cis.upenn.edu/index.html", url);
+//		}
+	}
 }
