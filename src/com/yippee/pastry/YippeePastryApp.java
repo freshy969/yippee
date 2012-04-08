ecaea7b0028e4163d3b88ea037420146ad2eea92
nvasilakis <nikos.ailo@gmail.com>
2012-04-08 14:00:18 -0400

    Fix @Override

--- src/com/yippee/pastry/YippeePastryApp.java
+++ src/com/yippee/pastry/YippeePastryApp.java
@@ -8,19 +8,16 @@ import rice.p2p.commonapi.RouteMessage;
 
 public class YippeePastryApp implements Application {
 
-	@Override
 	public void deliver(Id arg0, Message arg1) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public boolean forward(RouteMessage arg0) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public void update(NodeHandle arg0, boolean arg1) {
 		// TODO Auto-generated method stub
 
