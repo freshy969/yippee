97f8bc175ef2dc6cf652bd2fa2b86aab36ad6c2c
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-04-08 14:40:24 -0400

    Added existing classes to TestSuites (#4)

---
+++ test/com/yippee/pastry/PastryTestSuite.java
@@ -0,0 +1,11 @@
+package com.yippee.pastry;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({YippeePastryApp.class})
+public class PastryTestSuite {
+
+}
