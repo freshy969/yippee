4d0dac923e2478669edbb5e773024bed66dc51c5
nvasilakis <nikos.ailo@gmail.com>
2012-04-10 22:07:09 -0400

    Add ring message class (PastryMessage - #13)

---
+++ src/com/yippee/pastry/PastryMessage.java
@@ -0,0 +1,43 @@
+package com.yippee.pastry;
+
+import rice.p2p.commonapi.Message;
+import rice.p2p.commonapi.NodeHandle;
+
+/**
+ * An implementation of the pastry application message sent around nodes.
+ */
+public class PastryMessage implements Message {
+    /**
+     * The handle of the node from which the message was sent
+     * */
+    NodeHandle from;
+    /**
+     * The actual content of the message
+     * */
+    String content;
+    /**
+     * The response of the message
+     */
+    boolean wantResponse = true;
+
+    /**
+     * The message constructor (needs to keep the handle from which it was sent
+     * and the actual content to deliver)
+     *
+     * @param from the handle of the node from which it was sent
+     * @param content the actual content to deliver
+     */
+    public PastryMessage(NodeHandle from, String content) {
+        this.from = from;
+        this.content = content;
+    }
+
+    /**
+     * The priority of the message delivery/route. This is set to default value.
+     *
+     * @return the priority
+     */
+    public int getPriority() {
+        return 0;
+    }
+}
