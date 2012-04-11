78fb96c4bd4b566601744e0c524159c7ca5e8581
nvasilakis <nikos.ailo@gmail.com>
2012-04-10 21:14:53 -0400

    Update tests (#23, #24, #25)

--- test/com/yippee/indexer/IndexerTestSuite.java
+++ test/com/yippee/indexer/IndexerTestSuite.java
@@ -7,5 +7,9 @@ import org.junit.runners.Suite;
  * Include all tests of the Indexer Package
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({com.yippee.indexer.MessageTest.class})
+@Suite.SuiteClasses({   MessageTest.class,
+                        IndexerTest.class,
+                        LexiconTest.class,
+                        WordStemmer.class
+})
 public class IndexerTestSuite {}
