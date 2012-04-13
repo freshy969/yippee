6c0114a398d8215d98dc142f28cb9d9a18781180
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-04-13 13:25:48 -0400

    Failing test due to format of URL in Message

--- test/com/yippee/crawler/frontier/SimpleQueueFrontierTest.java
+++ test/com/yippee/crawler/frontier/SimpleQueueFrontierTest.java
@@ -2,23 +2,31 @@ package com.yippee.crawler.frontier;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.yippee.crawler.Message;
+
 public class SimpleQueueFrontierTest {
+	SimpleQueueFrontier frontier;
 
 	@Before
 	public void setUp() throws Exception {
+		frontier = new SimpleQueueFrontier();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPushPullOneItem() throws MalformedURLException, InterruptedException{
+		String url = "http://crawltest.cis.upenn.edu";
+		frontier.push(new Message(url));
+		assertEquals(url, frontier.pull().getURL());
 	}
 
 }
