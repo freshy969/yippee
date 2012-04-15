fcedddc35562145fe1ed6130f4446ba9480a7fb8
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-04-15 18:12:32 -0400

    Added SimpleQueueFrontierTest

--- test/com/yippee/crawler/CrawlerTestSuite.java
+++ test/com/yippee/crawler/CrawlerTestSuite.java
@@ -3,11 +3,14 @@ package com.yippee.crawler;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.yippee.crawler.frontier.SimpleQueueFrontierTest;
+
 /**
  * Include all tests of the Crawler Package
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({	MessageTest.class,
 						HttpModuleTest.class,
-						RobotsModuleTest.class})
+						RobotsModuleTest.class,
+						SimpleQueueFrontierTest.class})
 public class CrawlerTestSuite { }
