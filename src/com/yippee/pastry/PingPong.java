b312a1c31ce766f696a1117021b8565a07b72dfa
nvasilakis <nikos.ailo@gmail.com>
2012-04-16 18:27:40 -0400

    Add ping-pong service (#13)

---
+++ src/com/yippee/pastry/PingPong.java
@@ -0,0 +1,44 @@
+package com.yippee.pastry;
+
+import com.yippee.util.Configuration;
+
+/**
+ * This class implements the ping-pong thread, necessary to
+ * make sure that all nodes work together.
+ *
+ * boot-up 10 seconds, ping pong every 5 seconds
+ */
+public class PingPong implements Runnable {
+    /**
+     * Toggle whether the node is booting or not.
+     */
+    private boolean booting;
+
+    /**
+     * Constructor -- when object is constructed, starts on the booting process
+     */
+    public PingPong() {
+        booting = true;
+    }
+
+    /**
+     * The overridden thread run method
+     */
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                if (booting) {
+                    Thread.sleep(10000);
+                    booting = false;
+                } else {
+                    Thread.sleep(5000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();  // No need to reach here
+            }
+            // generate random
+            Configuration.getInstance().getPastryEngine().sendPing();
+        }
+
+    }
+}
