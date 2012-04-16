edbad799573e776c43223d3b863171a7a17d8ba0
nvasilakis <nikos.ailo@gmail.com>
2012-04-16 19:14:18 -0400

    Add global entry point (in util) (#35)

--- src/com/yippee/pastry/YippeeEngine.java
+++ src/com/yippee/pastry/YippeeEngine.java
@@ -1,6 +1,5 @@
 package com.yippee.pastry;
 
-import com.yippee.util.Configuration;
 import org.apache.log4j.Logger;
 
 import java.net.InetAddress;
@@ -51,41 +50,5 @@ public class YippeeEngine {
      */
     public void sendPing(){
         yippeePastryApp.send(nodeFactory.nidFactory.generateNodeId(), "PING");
-    }
-
-
-    /**
-     * The entry point for the whole Yippee Engine. It takes the following
-     * arguments:
-     * <p/>
-     * 1.   The port number on the local machine to which the Pastry node
-     * should bind;
-     * 2.   The IP address of the Pastry bootstrap node;
-     * 3.   The port number of the Pastry bootstrap node;
-     *
-     * @param args  The command line arguments at the order specified above, for
-     *              instance 9001 130.91.140.235 9001 4444 DB/db1
-     */
-    public static void main(String[] args) {
-        if (args.length<0) {
-            System.out.println("There are no arguments;");
-            System.out.println("please check README file, or run 'ant usage'");
-            logger.error("Error: No arguments");
-            return;
-        }
-
-        System.out.println("2012, Yippee!");
-        // TODO: THESE ARE SOME OF THE CONFIGURATIONS NEED TO BE DONE
-        System.out.println("TODO: \n\t * set Thread number \n\t * set Daemon port");
-        final int NO_OF_THREADS = 1;
-        final String BERKELEY_DB = "db/test";
-
-        YippeeEngine yippeeEngine = new YippeeEngine(Integer.parseInt(args[0]),
-                args[1], Integer.parseInt(args[2]), args[4]);
-
-        Configuration.getInstance().setPastryEngine(yippeeEngine);
-        Configuration.getInstance().setThreadNumber(NO_OF_THREADS);
-        Configuration.getInstance().setBerkeleyDB(args[4]);
-
     }
 }
---
+++ src/com/yippee/util/EntryPoint.java
@@ -0,0 +1,13 @@
+package com.yippee.util;
+
+/**
+ * This the entry point of the Yippe back-end engine, providing convenient setup
+ * for the whole application. Among others, it creates the Pastry ring, the configuration
+ * environment, the crawler, the database environment and more.
+ */
+public class EntryPoint {
+
+
+
+
+}
