85a4ffa65c789b2f6be53b573dfdf9b26856b60c
nvasilakis <nikos.ailo@gmail.com>
2012-04-16 20:41:16 -0400

    Rmoving "@Override"s from SimpleQueueFrontier

--- src/com/yippee/crawler/frontier/SimpleQueueFrontier.java
+++ src/com/yippee/crawler/frontier/SimpleQueueFrontier.java
@@ -1,9 +1,9 @@
 package com.yippee.crawler.frontier;
 
+import com.yippee.crawler.Message;
+
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
-
-import com.yippee.crawler.Message;
 
 public class SimpleQueueFrontier implements URLFrontier {
 	
@@ -13,25 +13,21 @@ public class SimpleQueueFrontier implements URLFrontier {
 		urls = new PriorityBlockingQueue<Message>();
 	}
 
-	@Override
 	public Message pull() throws InterruptedException {
 		// TODO Auto-generated method stub
 		return urls.take();
 	}
 
-	@Override
 	public void push(Message message) {
 		// TODO Auto-generated method stub
 		urls.add(message);
 	}
 
-	@Override
 	public boolean save() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean load() {
 		// TODO Auto-generated method stub
 		return false;
