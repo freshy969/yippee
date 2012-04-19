2f988deabfa0e211994400fd03df2b73dbb39612
nvasilakis <nikos.ailo@gmail.com>
2012-04-19 01:19:13 -0400

    Update backup scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,7 +9,6 @@
 # Usage: ./backup.sh
 ##
 
-
 ##
 #Function used for appending log to a file
 ##
@@ -17,5 +16,16 @@ function e {
   echo "$1" >> ~/.backup.log
 }
 
-e "# Backup log of $(date)"
+username=#user#;
+direname=#dire#;
 
+cd $direname;
+dt=`date | sed 's/ /-/g'`;
+filename=snapshot-$dt.tar.bz2
+e
+e "# Backup log of $dt"
+cd db;
+e "sending $filename to $username@eniac.seas.upenn.edu:~/"
+tar -cjf ../$filename * >> ~/.backup.log
+cd ../
+rsync -av --progress ./$filename $username@eniac.seas.upenn.edu:~/ >> ~/.backup.log
--- scripts/install.sh
+++ scripts/install.sh
@@ -30,6 +30,9 @@ if [[ $1 != 'nvas' ]]; then
   exit;
 fi
 
+cd ../
+PROJECTROOT=`pwd`;
+cd scripts;
 e 'generating key pair..'
 e 'enter your ssh password at EC2 and leave next fields blank!'
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -C `whoami`@`uname -n`
@@ -38,10 +41,16 @@ e 'enter your eniac ssh password'
 ssh-copy-id -i $1@eniac.seas.upenn.edu
 e 'adding pair to ssh agent..'
 ssh-add ~/.ssh/id_rsa
+e 'setting up backup scripts..'
+cp backup.sh ~/
+sed -i "s;#user#;$1;" ~/backup.sh;
+sed -i "s;#dire#;$PROJECTROOT;" ~/backup.sh;
+#sed -i "s/dire/$PROJECTROOT/" ~/backup.sh;
 # we could rsync to make sure that it works
-e 'Now logging to '$1'@eniac.seas.upenn.edu ..'
+e 'logging to '$1'@eniac.seas.upenn.edu should not require password..'
 e 
 e '******************************************'
-e '*This should *not* request your password!*'
+e '***  type exit or <control>+D to quit  ***'
 e '******************************************'
-echo "# Log file generated automatically on $(date)" > ~/.backup.log
+echo "# Log file for project in $PROJECTROOT generated on $(date)" > ~/.backup.log
+ssh $1@eniac.seas.upenn.edu
