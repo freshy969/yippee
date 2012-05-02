edf8af6eab05f9e2ae560bb47a0f44120f00befb
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-05-01 22:28:17 -0400

    Forgot to track this file

---
+++ src/com/yippee/crawler/RobotsTxtCache.java
@@ -0,0 +1,29 @@
+package com.yippee.crawler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.yippee.db.crawler.model.RobotsTxt;
+import com.yippee.util.Configuration;
+
+public class RobotsTxtCache extends LinkedHashMap<String, RobotsTxt> {
+	/**
+     * Create logger in the Log4j hierarchy named by by software component
+     */
+    static Logger logger = Logger.getLogger(RobotsTxtCache.class);
+	private static final long serialVersionUID = -3181513645658365078L;
+	private int capacity;
+	
+	public RobotsTxtCache(){
+		this.capacity = Configuration.getInstance().getRobotsCacheSize();
+	}
+	
+	@Override
+	public boolean removeEldestEntry(Map.Entry<String, RobotsTxt> eldest) {
+		boolean result = this.size() > capacity;
+		if(result) logger.info("Cache eviction");
+		return result;
+	}
+}
