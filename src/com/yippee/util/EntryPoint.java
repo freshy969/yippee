2cb81bf286f779f63ddab8c2eff6ea189c9be879
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-05-02 19:33:08 -0400

    MOdified size of robots cache

--- src/com/yippee/util/EntryPoint.java
+++ src/com/yippee/util/EntryPoint.java
@@ -39,7 +39,7 @@ public class EntryPoint {
      * TODO: THESE NEED TO BE GIVEN DYNAMICALLY -- this is where caution message applies to.
      */
     final int NO_OF_THREADS = 1;
-    final int SIZE_OF_ROBOTS_CACHE = 100;
+    final int SIZE_OF_ROBOTS_CACHE = 512;
     
     /**
      * The default constructor does the minimum of setting up the logger
