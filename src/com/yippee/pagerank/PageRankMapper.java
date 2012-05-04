c13cacc102304d7c2fd140272c228c2ecf570f42
nvasilakis <nikos.ailo@gmail.com>
2012-05-04 05:36:11 -0400

    Update PageRankMapper

--- src/com/yippee/pagerank/PageRankMapper.java
+++ src/com/yippee/pagerank/PageRankMapper.java
@@ -16,8 +16,8 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text>{
 
         String[] line = value.toString().split(", ");
         if (line.length>2) {
-            String from = line[0];
-            String to   = line[1];
+            String from = line[0].substring(1,line[0].length()-1);
+            String to   = line[1].substring(1,line[0].length()-1);
             context.write(new Text(to), new Text(from));
         }
     }
