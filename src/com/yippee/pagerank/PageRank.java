448b568ebe49fda04efab6fb0bfd7cddc71379d0
nvasilakis <nikos.ailo@gmail.com>
2012-05-04 05:09:01 -0400

    Update PageRank

---
+++ src/com/yippee/pagerank/PageRank.java
@@ -0,0 +1,47 @@
+package com.yippee.pagerank;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import javax.xml.soap.Text;
+import java.io.IOException;
+
+/**
+ * The MapReduce Configuration class, responsible for setting up the environment,
+ * configuring inputs and outputs and launching the job.
+ */
+public class PageRank{
+
+    /**
+     * The constructor is responsible for configuring the job.
+     */
+    public PageRank(String input, String output) throws IOException,
+            ClassNotFoundException, InterruptedException {
+        Job job = new Job();
+        job.setJarByClass(PageRank.class);
+
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+
+        job.setMapperClass(PageRankMapper.class);
+        job.setReducerClass(PageRankReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true)? 0:1);
+    }
+
+    public static void main(String[] args) throws  Exception{
+        if (args.length!=2) {
+            System.out.println("Usage: PageRank <input> <output>");
+        }
+        PageRank pr = new PageRank(args[0], args[1]);
+
+    }
+
+
+}
