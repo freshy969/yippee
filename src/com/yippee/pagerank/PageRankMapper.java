efe58684dfe6e0a82a6c8e93d29fccb82276b48a
nvasilakis <nikos.ailo@gmail.com>
2012-05-04 05:00:30 -0400

    Add a preliminary Mapper

--- src/com/yippee/pagerank/PageRankMapper.java
+++ src/com/yippee/pagerank/PageRankMapper.java
@@ -1,6 +1,5 @@
 package com.yippee.pagerank;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -10,11 +9,16 @@ import java.io.IOException;
 /**
  * This is the Mapper class, following the MapReduce programming model.
  */
-public class PageRankMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text>{
 
     public void map(LongWritable key, Text value, Context context) throws
             IOException, InterruptedException {
+
+        String[] line = value.toString().split(", ");
+        if (line.length>2) {
+            String from = line[0];
+            String to   = line[1];
+            context.write(new Text(to), new Text(from));
+        }
     }
-
-
 }
