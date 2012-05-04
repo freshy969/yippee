c9b64c774122d9035a146d2d841b52290fb8af52
nvasilakis <nikos.ailo@gmail.com>
2012-05-03 21:42:07 -0400

    Update ping pong handling

--- src/com/yippee/pastry/YippeePastryApp.java
+++ src/com/yippee/pastry/YippeePastryApp.java
@@ -30,8 +30,8 @@ public class YippeePastryApp implements Application {
 
     private BarrelManager barrelManager;
 
-	private SocketQueue queryQueue;
-    
+    private SocketQueue queryQueue;
+
     /**
      * Constructor
      *
@@ -42,118 +42,90 @@ public class YippeePastryApp implements Application {
         node = nodeFactory.getNode();
         endpoint = node.buildEndpoint(this, "Yippee App");
         barrelManager = new BarrelManager();
-       // endpoint.accept(new PastryAppSocketReceiver(node, endpoint));
+        // endpoint.accept(new PastryAppSocketReceiver(node, endpoint));
         endpoint.register();
-		queryQueue = new SocketQueue(100);
+        queryQueue = new SocketQueue(100);
     }
 
-    public void setupURLFrontier(URLFrontier urlFrontier){
+    public void setupURLFrontier(URLFrontier urlFrontier) {
         this.urlFrontier = urlFrontier;
     }
-    
-	/**
-	 * Starts the daemon thread
-	 * 
-	 * @param port port on which the daemon listens
-	 */
-	public void startDaemonListener(int port) {
-		Thread daemon = new Thread(new DaemonListener(port, queryQueue));
-		daemon.setDaemon(true);
-		daemon.start();
-	}
-    
+
+    /**
+     * Starts the daemon thread
+     *
+     * @param port port on which the daemon listens
+     */
+    public void startDaemonListener(int port) {
+        Thread daemon = new Thread(new DaemonListener(port, queryQueue));
+        daemon.setDaemon(true);
+        daemon.start();
+    }
+
     /**
      * Called when the Pastry application receives a message. It pushes the url
      * to the URLFrontier (maybe through a duplicate URL eliminator).
      */
-	public void deliver(Id targetId, Message message) {
-		
-		if(message instanceof CrawlerMessage){
-			handleCrawlerMessage(targetId, (CrawlerMessage) message);
-		} else if(message instanceof IndexerMessage){
-			handleIndexerMessage(targetId, (IndexerMessage) message);
-		} else if(message instanceof QueryMessage){
-			handleQueryMessage(targetId, (QueryMessage) message);
-		} else if(message instanceof PingPongMessage){
-			handlePingPongMessage(targetId, (PingPongMessage) message);
-		}else {
-			logger.error("Unknown pastry message received!");
-			logger.error(message);
-		}
+    public void deliver(Id targetId, Message message) {
 
-	}
+        if (message instanceof CrawlerMessage) {
+            handleCrawlerMessage(targetId, (CrawlerMessage) message);
+        } else if (message instanceof IndexerMessage) {
+            handleIndexerMessage(targetId, (IndexerMessage) message);
+        } else if (message instanceof QueryMessage) {
+            handleQueryMessage(targetId, (QueryMessage) message);
+        } else if (message instanceof PingPongMessage) {
+            handlePingPongMessage(targetId, (PingPongMessage) message);
+        } else {
+            logger.error("Unknown pastry message received!");
+            logger.error(message);
+        }
 
-  
-    private void handlePingPongMessage(Id targetId, PingPongMessage message) {
-		// TODO Auto-generated method stub
-    	
-		
-		
-/*		
-        PastryMessage om = (PastryMessage) message;
-        logger.debug("Received message " + om.content + " from " + om.from);
-        if (om.wantResponse) { // if it is a query
+    }
 
-            if (om.content.equals("PING")) {
-                logger.debug("Received PING to ID " + targetId + " from node " +
-                        om.from.getId() + "; returning PONG");
-                sendDirect(om.from, "PONG");
 
     private void handlePingPongMessage(Id id, PingPongMessage message) {
-		if (message.isWantResponse()) { // if it is a query
+        if (message.isWantResponse()) { // if it is a query
             if (message.getContent().equals("PING")) {
                 logger.debug("Received PING to ID " + id + " from node " +
                         message.getFrom().getId() + "; returning PONG");
                 sendDirect(message.getFrom(), "PONG");
-
             } else {// else for other queries
             }
         } else {
-
-            if (om.content.equals("PONG")) {
-                logger.debug("Received PONG from node " + om.from.getId());
-            } else if(om.hitList.size()>0) { //message with hitlist in it
-        		logger.info("Saving in barrels");
-        		barrelManager.addDocHits(om.hitList);
-        	}
-
             if (message.getContent().equals("PONG")) {
                 logger.debug("Received PONG from node " + message.getFrom().getId());
             }
         }
+    }
 
-        }*/
-		
+    private void handleQueryMessage(Id targetId, QueryMessage message) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	private void handleQueryMessage(Id targetId, QueryMessage message) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void handleIndexerMessage(Id targetId, IndexerMessage message) {
-		logger.info("Saving in barrels");
-		barrelManager.addDocHits(message.getHitList());		
-	}
+    private void handleIndexerMessage(Id targetId, IndexerMessage message) {
+        logger.info("Saving in barrels");
+        barrelManager.addDocHits(message.getHitList());
+    }
 
 
     /**
      * Handle the crawler messages
      *
      * @param targetId the target node
-     * @param message the actual crawler message
+     * @param message  the actual crawler message
      */
-	private void handleCrawlerMessage(Id targetId, CrawlerMessage message) {
-		String urlString = message.getUrl();
-        logger.info("Pushing ["+ urlString +"] to the URLFRONTIER");
+    private void handleCrawlerMessage(Id targetId, CrawlerMessage message) {
+        String urlString = message.getUrl();
+        logger.info("Pushing [" + urlString + "] to the URLFRONTIER");
         com.yippee.crawler.Message msg = new com.yippee.crawler.Message(urlString);
-        if (msg.getType() == com.yippee.crawler.Message.Type.NEW){
+        if (msg.getType() == com.yippee.crawler.Message.Type.NEW) {
             urlFrontier.push(msg);
         }
-	}
+    }
 
-	/**
+    /**
      * Called to route a message to the id
      */
     void send(Id idToSendTo, String msgString) {
@@ -173,22 +145,22 @@ public class YippeePastryApp implements Application {
 
         logger.info(this + " sending direct to " + nh);
         PastryMessage message = new PastryMessage(node.getLocalNodeHandle(), msgString);
-   //     message.wantResponse = false;
+        //     message.wantResponse = false;
         endpoint.route(null, message, nh);
     }
-    
+
     public void sendSocketDirect(NodeHandle nh, ArrayList<Hit> list) {
-    	logger.info(this + " sending hit list direct to " + nh);
-        endpoint.connect(nh, new PastryAppSocketSender(node,endpoint,list), 30000);
+        logger.info(this + " sending hit list direct to " + nh);
+        endpoint.connect(nh, new PastryAppSocketSender(node, endpoint, list), 30000);
     }
-    
+
     public void sendList(Id idToSendTo, String word, ArrayList<Hit> list) {
-    	logger.info(this + " sending hit list for ["+word+"] to " + idToSendTo);
-    	IndexerMessage message = new IndexerMessage(node.getLocalNodeHandle(), word, list);
-    //	message.wantResponse = false;
+        logger.info(this + " sending hit list for [" + word + "] to " + idToSendTo);
+        IndexerMessage message = new IndexerMessage(node.getLocalNodeHandle(), word, list);
+        //	message.wantResponse = false;
         endpoint.route(idToSendTo, message, null);
-    }    
-    
+    }
+
 
     /**
      * This is always true in our application.
@@ -196,13 +168,14 @@ public class YippeePastryApp implements Application {
      * @param routeMessage a message
      * @return true always
      */
-	public boolean forward(RouteMessage routeMessage) {
-		return true;
-	}
+    public boolean forward(RouteMessage routeMessage) {
+        return true;
+    }
 
     /**
      * Called when we hear about a new neighbor.
      * We do not make use of this method for now.
      */
-	public void update(NodeHandle arg0, boolean arg1) {}
+    public void update(NodeHandle arg0, boolean arg1) {
+    }
 }
