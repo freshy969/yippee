7eb0d7e2cebfb284084ef629ca453a1533b96359
nvasilakis <nikos.ailo@gmail.com>
2012-05-05 01:16:26 -0400

    Update Mapper

--- src/com/yippee/pagerank/PageRank.java
+++ src/com/yippee/pagerank/PageRank.java
@@ -19,18 +19,27 @@ public class PageRank {
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, Text> {
 
-        private Text from = new Text();
-        private Text to = new Text();
+        private Text mapKey = new Text();
+        private Text incomingLinks = new Text();
+        private Text outgoingLinks = new Text();
+        private static String DEL = "', '";
 
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
-            String[] line = value.toString().split(", ");
+            String[] line = value.toString().split(DEL);
             if (line.length>1) {
-                String fromString = line[0].trim();
-                String toString   = line[1].trim();
-                from.set(fromString);
-                to.set(toString);
-                context.write(to, from);
+                String fromPage = line[0].trim();
+                String fromRank = line[1].trim();
+                String toPage = line[2].trim();
+                String toOutNum = line[3].trim();
+                // This is in order to calculate the pagerank of the 'to' page "C. Imbriano"
+                mapKey.set(toPage);
+                incomingLinks.set("'IN" + DEL + fromPage + DEL + fromRank + DEL + toOutNum + "'");
+                context.write(mapKey, incomingLinks);
+                // This is in order to keep track of outgoing links in order to
+                // scale each page-rank voting power.
+                outgoingLinks.set("'OUT" + DEL + fromPage + "'");
+                context.write(outgoingLinks, mapKey);
             }
         }
     }
