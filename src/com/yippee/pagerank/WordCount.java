5cf85507484e76c82868d1862ecbb09cf33fd8d1
nvasilakis <nikos.ailo@gmail.com>
2012-05-04 23:47:20 -0400

    Fully functional map-reduce

--- src/com/yippee/pagerank/WordCount.java
+++ src/com/yippee/pagerank/WordCount.java
@@ -9,61 +9,65 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+
+//import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, Text> {
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+        private Text from = new Text();
+        private Text to = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] line = value.toString().split(", ");
+            String fromString = line[0].trim();
+            String toString   = line[1].trim();
+            from.set(fromString);
+            to.set(toString);
+            context.write(to, from);
+        }
     }
-  }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+    public static class IntSumReducer
+            extends Reducer<Text, Text, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
+        public void reduce(Text key, Iterable<Text> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (Text val : values) {
+                sum ++;
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
     }
-  }
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    //String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (args.length != 3) {
-      System.err.println("Usage: wordcount <in> <out>");
-      System.exit(2);
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        //String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (args.length != 3) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+//        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        // Set the outputs for the Map
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-    Job job = new Job(conf, "word count");
-    job.setJarByClass(WordCount.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[1]));
-    FileOutputFormat.setOutputPath(job, new Path(args[2]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
 }
