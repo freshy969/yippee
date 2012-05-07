be35109f14f47f5eb5da2b0b8d4c24f212a4f5ea
TJ Du <tdu2@seas.upenn.edu>
2012-05-07 13:00:57 -0400

    AH

--- src/com/yippee/pastry/YippeePastryApp.java
+++ src/com/yippee/pastry/YippeePastryApp.java
@@ -471,7 +471,7 @@ public class YippeePastryApp implements Application {
 			out.println("<query>" + StringEscapeUtils.escapeXml(queryMap.get(queryID)) + "</query>");
 			
 			if (deList == null) {
-				out.println("<document><description>No matching documents found!</description></document>");
+				out.println("<size>0</size><time>0</time><document><title>No matching documents found!</time></document>");
 			} else {
 //				int min = page * 25;
 //				int max = (page + 1) * 25;
